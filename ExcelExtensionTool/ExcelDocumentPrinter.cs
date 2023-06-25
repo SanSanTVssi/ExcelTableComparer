ee38cf67f11b9747b2967d670e2f3400991b00d5
Oleksandr <35868682+SanSanTVssi@users.noreply.github.com>
2023-06-25 13:29:54 +0300

    Added Implementation for base table compare tool

---
+++ ExcelExtensionTool/ExcelDocumentPrinter.cs
@@ -0,0 +1,49 @@
+using OfficeOpenXml;
+
+namespace ExcelExtensionTool;
+
+public class ExcelDocumentPrinter
+{
+    public void PrintExcelTable(IEnumerable<IEnumerable<string>> table)
+    {
+        foreach (var rowData in table)
+        {
+            foreach (var cellData in rowData)
+            {
+                Console.Write(cellData + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public void PrintExcelTableToFile(IEnumerable<IEnumerable<string>> table, string fileName)
+    {
+        foreach (var rowData in table)
+        {
+            foreach (var cellData in rowData)
+            {
+                File.AppendAllText(fileName, cellData + "\t");
+            }
+            File.AppendAllText(fileName, "\n");
+        }
+    }
+
+    public void SaveTableToExcelDocument(string excelFile, string listName, List<List<string>> dataCollection)
+    {
+        using var package = new ExcelPackage();
+        var c = package.Workbook.Worksheets.Count;
+        ExcelWorksheet worksheet = package.Workbook.Worksheets.Add(listName);
+
+        for (int row = 0; row < dataCollection.Count; row++)
+        {
+            List<string> rowData = dataCollection[row];
+            for (int col = 0; col < rowData.Count; col++)
+            {
+                worksheet.Cells[row + 1, col + 1].Value = rowData[col];
+            }
+        }
+
+        FileInfo file = new FileInfo(excelFile);
+        package.SaveAs(file);
+    }
+}
---
+++ ExcelExtensionTool/ExcelDocumentReader.cs
@@ -0,0 +1,33 @@
+using OfficeOpenXml;
+
+namespace ExcelExtensionTool;
+
+public class ExcelDocumentReader
+{
+    public ExcelDocumentReader()
+    {
+        ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+    }
+
+    public List<List<string>> ReadExcelTable(string excelFile, string listName)
+    {
+        using var package = new ExcelPackage(new FileInfo(excelFile));
+        var workbook = package.Workbook;
+        var worksheet = workbook.Worksheets[listName];
+        var dataCollection = new List<List<string>>();
+
+        for (var row = 1; row <= worksheet.Dimension.Rows; row++)
+        {
+            var rowData = new List<string>();
+            for (var col = 1; col <= worksheet.Dimension.Columns; col++)
+            {
+                var cellValue = worksheet.Cells[row, col].Value;
+                var cellData = cellValue != null ? cellValue.ToString() : string.Empty;
+                rowData.Add(cellData);
+            }
+            dataCollection.Add(rowData);
+        }
+
+        return dataCollection;
+    }
+}
